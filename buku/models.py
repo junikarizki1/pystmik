f4a2de9e9e8482c885c405a94851c9c39a27fa3e
junikarizki1 <rizkijrstmik22@gmail.com>
2024-12-13 15:03:58 +0700

    penerbit terintegrasi ke buku

--- buku/models.py
+++ buku/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from penerbit.models import Publisher
 class Book(models.Model):
     kode_buku=models.CharField(max_length=4)
     judul_buku=models.CharField(max_length=100)
     pengarang=models.CharField(max_length=50)
-    penerbit=models.CharField(max_length=40)
+    penerbit= models.ForeignKey(Publisher, on_delete=models.CASCADE, default=1) 
     tahun=models.CharField(max_length=4)
 
     def __str__(self):
         return self.judul_buku
+# penerbit agak beda supaya terintegrasi, biar pas  input di buku, penerbitnya langsung bisa dipilih
